5b315d23be3d844c1eee9ee5d0aaf275fe0ed7d9
Cihan TAYLAN <cihantaylan@cihantaylan.com>
2022-12-15 09:47:46 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -4,57 +4,36 @@ export default function App() {
   const [points, setPoints] = useState([])
   const [data, setData] = useState([])
   const clickHandle = e => {
-    setPoints(points => [  ...points,
-      {
-        x: e.clientX,
-        y: e.clientY
-      }
-    ])
-    setData([])
+    setPoints(points => [...points, { x: e.clientX, y: e.clientY }]);
+    setData([]);
   }
   const keyDownHandle = event => {
     event.preventDefault();
     let charCode = String.fromCharCode(event.which).toLowerCase();
-    if((event.ctrlKey || event.metaKey) && event.shiftKey && charCode === 'z') {
-      if (data.length ) {
-      ileriHandle(event)
-        
-      }
-    } else if( (event.ctrlKey || event.metaKey) && charCode === 'z') {
-      if (points.length) {
-      geriHandle(event)
-        
-      }
-    }
+    if ((event.ctrlKey || event.metaKey) && event.shiftKey && charCode === 'z') if (data.length) redoHandle(event);
+    else if ((event.ctrlKey || event.metaKey) && charCode === 'z') if (points.length) undoHandle(event);
   }
-  const geriHandle = e => {
-    let charCode=String.fromCharCode(e.which).toLowerCase();
-    e.stopPropagation()
-    const data = [...points]
-    const item = data.pop()
-    setData(data => [...data, item])
-    setPoints(data)
+  const undoHandle = e => {
+    e.stopPropagation();
+    const data = [...points];
+    const item = data.pop();
+    setData(data => [...data, item]);
+    setPoints(data);
   }
-  const ileriHandle = e => {
-    e.stopPropagation()
-    const d = [...data]
-    const item = d.pop()
-    setPoints(points => [...points, item])
-    setData(d)
+  const redoHandle = e => {
+    e.stopPropagation();
+    const d = [...data];
+    const item = d.pop();
+    setPoints(points => [...points, item]);
+    setData(d);
   }
   return (
     <div className="screen" onClick={clickHandle} onKeyDown={keyDownHandle} contentEditable={true} suppressContentEditableWarning={true}>
       <header className="header">
-        <button disabled={points.length === 0} onClick={geriHandle} contentEditable={false}>
-          Geri
-        </button>
-        <button disabled={data.length === 0} onClick={ileriHandle} contentEditable={false}>
-          İleri
-        </button>
+        <button disabled={points.length === 0} onClick={undoHandle} contentEditable={false}>Undo</button>
+        <button disabled={data.length === 0} onClick={redoHandle} contentEditable={false}>Redo</button>
       </header>
-      {points.map((point, key) => (
-        <div className="point" key={key} style={{ '--left': point.x +'px', '--top': point.y +'px'}}/>
-      ))}
+      {points.map((point, key) => <div className="point" key={key} style={{ '--left': point.x + 'px', '--top': point.y + 'px' }} />)}
     </div>
   )
 }
